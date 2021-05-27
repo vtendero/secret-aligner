25717d1b4192ad8e29e33990b92d0eefccb709ab
vtendero <tendero.valle@gmail.com>
2021-05-27 11:17:15 +0200

    Initial commit

---
+++ my-app/src/components/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import Header from './Header';
+import '../styles/App.css';
+
+const App = () => {
+
+    return (
+      <>
+        <Header />
+      </>
+    );
+}
+
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => 
+        <header className= 'header'>
+            <div className='header__user'>
+                <i className='far fa-user header__user--icon'></i>
+                <h4 className='header__user--name'>Dr. Pablo Kehyaian</h4>
+                <button className='fas fa-angle-down header__user--button'></button>
+            </div>
+        </header>
+
+export default Header;
